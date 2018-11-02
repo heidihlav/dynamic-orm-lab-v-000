78576b051fb7ed0fd6281e2aa5799a06f94b4e29
Heidi Hlavinka <hchlav@gmail.com>
2018-11-02 18:38:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,7 @@ class InteractiveRecord
 
   def self.find_by(attr)
     #convert integer to string - maybe if/else statement
+    values = attr.values.flatten[0]
     sql = "SELECT * FROM #{self.table_name} WHERE #{attr.keys.flatten[0].to_s} = #{attr.values.flatten[0].to_s}"
     DB[:conn].execute(sql)
     # binding.pry
